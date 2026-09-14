951dec25a877d314d3220e274c92fba1204c31b2
Adrian James Famadulan <ajfamadulan8@gmail.com>
2026-09-14 09:30:40 +0800

    lab5

--- app/config/routes.php
+++ app/config/routes.php
@@ -54,9 +54,29 @@ $router->get('/login', 'AuthController::login');
 $router->post('/login', 'AuthController::authenticate');
 $router->get('/logout', 'AuthController::logout');
 
-$router->get('/products',             'ProductController::index',        ['middleware' => 'auth']);
-$router->get('/products/create',      'ProductController::create',       ['middleware' => 'auth']);
-$router->post('/products/store',      'ProductController::store',        ['middleware' => 'auth']);
-$router->get('/products/edit/{id}',   'ProductController::edit',         ['middleware' => 'auth']);
-$router->post('/products/update/{id}','ProductController::update',       ['middleware' => 'auth']);
-$router->get('/products/delete/{id}', 'ProductController::delete',       ['middleware' => 'auth']);
+
+$router->get('/products', 'ProductController::index', [
+    'middleware' => 'auth'
+]);
+
+$router->get('/products/create', 'ProductController::create', [
+    'middleware' => 'auth'
+]);
+
+$router->post('/products/store', 'ProductController::store', [
+    'middleware' => 'auth'
+]);
+
+$router->get('/products/edit/{id}', 'ProductController::edit', [
+    'middleware' => 'auth'
+]);
+
+$router->post('/products/update/{id}', 'ProductController::update', [
+    'middleware' => 'auth'
+]);
+
+$router->get('/products/delete/{id}', 'ProductController::delete', [
+    'middleware' => 'auth'
+]);
+
+
--- app/middlewares/AuthMiddleware.php
+++ app/middlewares/AuthMiddleware.php
@@ -3,19 +3,19 @@ defined('PREVENT_DIRECT_ACCESS') OR exit('No direct script access allowed');
 
 class AuthMiddleware
 {
-   public function handle(Closure $next)
+    public function handle(Closure $next)
     {
-        $lava = lava_instance();
-
-        $lava->call->library('session');
-
-        if (!$lava->session->userdata('logged_in')) {
-
-            redirect('login');
-
-            return;
+        if (session_status() === PHP_SESSION_NONE) {
+            session_start();
         }
 
+        // User is NOT logged in
+        if (!isset($_SESSION['logged_in']) || $_SESSION['logged_in'] !== true) {
+            header('Location: /login');
+            exit;
+        }
+
+        // User is logged in
         return $next();
     }
 }
